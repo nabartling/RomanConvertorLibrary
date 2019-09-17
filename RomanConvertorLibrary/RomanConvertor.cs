eb5426e12ca825e2adef76836215c977c81b10f4
nabartling <nbart5495@gmail.com>
2019-09-16 22:42:20 -0400

    passing test cases

--- RomanConvertorLibrary/RomanConvertor.cs
+++ RomanConvertorLibrary/RomanConvertor.cs
@@ -11,15 +11,76 @@ namespace RomanConvertorLibrary
 
 		public static int ToArabic(String roman)
 		{
-			// Replace the following line with the actual code!
-			throw new NotImplementedException();
-		}
+            // Replace the following line with the actual code!
+            
+            if (CharValues == null)
+            {
+                CharValues = new Dictionary<char, int>();
+                CharValues.Add('I', 1);
+                CharValues.Add('V', 5);
+                CharValues.Add('X', 10);
+                CharValues.Add('L', 50);
+                CharValues.Add('C', 100);
+                CharValues.Add('D', 500);
+                CharValues.Add('M', 1000);
+            }
+
+            if (roman.Length == 0) return 0;
+            roman = roman.ToUpper();
+
+            
+            if (roman[0] == '(')
+            {
+                int pos = roman.LastIndexOf(')');
+
+                string part1 = roman.Substring(1, pos - 1);
+                string part2 = roman.Substring(pos + 1);
+                return 1000 * RomanToArabic(part1) + RomanToArabic(part2);
+            }
+
+            int total = 0;
+            int last_value = 0;
+            for (int i = roman.Length - 1; i >= 0; i--)
+            {
+                int new_value = CharValues[roman[i]];
+
+                
+                if (new_value < last_value)
+                    total -= new_value;
+                else
+                {
+                    total += new_value;
+                    last_value = new_value;
+                }
+            }
+
+            
+            return total;
+        }
+
+        //throw new NotImplementedException();
+    }
 
 
 		public static String ToRoman(int arabic)
 		{
-			// Replace the following line with the actual code!
-			throw new NotImplementedException();
-		}
+            if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("insert value between 1 and 3999");
+            if (number < 1) return string.Empty;
+            if (number >= 1000) return "M" + ToRoman(number - 1000);
+            if (number >= 900) return "CM" + ToRoman(number - 900);
+            if (number >= 500) return "D" + ToRoman(number - 500);
+            if (number >= 400) return "CD" + ToRoman(number - 400);
+            if (number >= 100) return "C" + ToRoman(number - 100);
+            if (number >= 90) return "XC" + ToRoman(number - 90);
+            if (number >= 50) return "L" + ToRoman(number - 50);
+            if (number >= 40) return "XL" + ToRoman(number - 40);
+            if (number >= 10) return "X" + ToRoman(number - 10);
+            if (number >= 9) return "IX" + ToRoman(number - 9);
+            if (number >= 5) return "V" + ToRoman(number - 5);
+            if (number >= 4) return "IV" + ToRoman(number - 4);
+            if (number >= 1) return "I" + ToRoman(number - 1);
+            // Replace the following line with the actual code!
+            //throw new NotImplementedException();
+        }
 	}
 }
